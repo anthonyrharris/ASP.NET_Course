560eff5ef7574db3eab40bd7c25178c3714c1c89
anthonyrharris <tony.harris.business@gmail.com>
2020-09-21 16:50:38 -0400

    working on the booklist project

---
+++ BookListRazor/BookListRazor/Model/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        //automatically add id as an identity column
+        [Key]
+        public int Id { get; set; }
+
+        //name can't be null
+        [Required]
+        public int Name { get; set; }
+        public String Author { get; set; }
+    }
+}
